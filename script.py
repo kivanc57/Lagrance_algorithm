416f0a3dd150e5fc82602e831f7d23b93b79dae5
kivanc57 <kivancgordu@hotmail.com>
2024-01-18 19:04:54 +0300

    Final
    ENDEDthe Lagrance function.
    
    ADDED steps and print functions.
    
    ADDED matplotplotlib and numpy to vizualise the steps.

--- script.py
+++ script.py
@@ -1,76 +1,84 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import copy
-import random
 
-
-def laplace(any_list):
-    new_list = copy.deepcopy(any_list)
-    square_root = int(len(any_list) ** 0.5)
+# Step 1: Identify and replace ['.'] values with numerical ['0'] values
+def enumerate_list(any_list):
     special_indexes = []
 
-    # Step 1: Identify and replace ['.'] values with numerical values
-    for i in range(len(any_list)):
-        if any_list[i] == ['.']:
-            # Replace ['.'] with 0
-            new_list[i] = [0]
-            special_indexes.append(i)
+    if ['.'] in any_list:
+        for i in range(len(any_list)):
+            if any_list[i] == ['.']:
+                # Replace ['.'] with 0
+                any_list[i] = [0]
+                special_indexes.append(i)
+    return any_list, special_indexes
 
-    # Step 2: Perform Laplace smoothing on adjacent numerical values
-    while True:
-        for i in special_indexes:
-            neighbour_list = [0, 0, 0, 0]
-            neighbour_amount = 0
-            total = 0
-                
-            #Check if the left neighbour is available
-            if i != 0:
-                if i % square_root != 0:
-                    if any_list[i - 1] != ['.']:
-                        neighbour_list[0] = any_list[i - 1][0]
-                        total += any_list[i - 1][0]
-                        neighbour_amount += 1
+# Step 2: Perform Lagrance smoothing on adjacent numerical values
+def set_lagrance(any_list, special_indexes=[]):
+    new_list = copy.deepcopy(any_list)
+    square_root = int(len(any_list) ** 0.5)
 
-            ##Check if the right neighbour is available
-            if (i + 1) % square_root != 0:
-                if any_list[i + 1] != ['.']:
-                    neighbour_list[1] = any_list[i + 1][0]
-                    total += any_list[i + 1][0]
-                    neighbour_amount += 1
+    for i in special_indexes:
+        total = []
+        #Check the neighbours if they are not BORDERs and ZEROs,
+        #If not, add to list 'total' to calculate later
 
-            #Check if the upper neighbour is available
-            if square_root <= i:
-                if any_list[i - square_root] != ['.']:
-                    neighbour_list[2] = any_list[ i - square_root ][0]
-                    total += any_list[ i - square_root ][0]
-                    neighbour_amount += 1
-                    
-            ##Check if the lower neighbour is available
-            if i + square_root < len(any_list):
-                if any_list[i + square_root] != ['.']:
-                    neighbour_list[3] = any_list[ i + square_root][0]
-                    total += any_list[ i + square_root][0]
-                    neighbour_amount += 1
+        # Check left neighbour
+        if i != 0 and i % square_root != 0 and any_list[i - 1][0] != 0:
+            total.append(any_list[i - 1][0])
 
-            #Replace the element by the total / neighbour_amount and round it to have one decimal
-            new_list[i] = [round(total / neighbour_amount, 2)]
+        # Check right neighbour
+        if (i + 1) % square_root != 0 and any_list[i + 1][0] != 0:
+            total.append(any_list[i + 1][0])
 
+        # Check upper neighbour
+        if square_root <= i and any_list[i - square_root][0] != 0:
+            total.append(any_list[i - square_root][0])
 
+        # Check lower neighbour
+        if i + square_root < len(any_list) and any_list[i + square_root][0] != 0:
+            total.append(any_list[i + square_root][0])
 
+        # Replace the element by the sum divided by length and round it to one decimal
+        new_list[i] = [round(sum(total) / len(total), 1)]
 
-        
+    return new_list, special_indexes
+
+# Determine the shape based on the length of the data, visualize it
+def make_image(data):
+    side_length = int(np.sqrt(len(data)))
+    
+    data = np.reshape(data, (side_length, -1))
+
+    if 0 in data:
+        data[data == 0] = np.nan
+
+    plt.imshow(data, interpolation='none', cmap='viridis')
+    plt.show()
+
 
 def main():
     my_list = [
-    [5.0], [4.0], [4.0], [3.0],
-    [2.0], ['.'], ['.'], [1.0],
-    [1.0], ['.'], ['.'], [3.0],
-    [3.0], [3.0], [2.0], [4.0]
+        [5.0], [4.0], [4.0], [3.0],
+        [2.0], ['.'], ['.'], [1.0],
+        [1.0], ['.'], ['.'], [3.0],
+        [3.0], [3.0], [2.0], [4.0]
     ]
 
-    given_row_col = 10
-    #Later get the number with user input() and generate the list randomly
+    rendered_list, indexes = enumerate_list(my_list)
+    print('The list is tidied up and rendered from ZEROs')
 
-    laplace(my_list)
+    for step in range(1, 100):  # Limit the number of steps to avoid infinite loop
+        print(f'This is STEP: {step}')
+        make_image(np.array(rendered_list))
+        new_list, indexes = set_lagrance(any_list=rendered_list, special_indexes=indexes)
 
+        if new_list == rendered_list:
+            print(f'No more changes! Final STEP: {step}')
+            break
+
+        rendered_list = new_list
 
 if __name__ == '__main__':
     main()
